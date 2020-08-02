c647762c0a03d5fa74215613ef077b5b9321c019
Quinton Ashley <qashto@gmail.com>
2020-08-02 07:18:09 -0700

    1.0.2

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+#!/usr/bin/env node
+
+const log = console.log;
 const deepExtend = require('deep-extend');
 const fs = require('fs');
 
-let json0 = JSON.parse(fs.readFileSync(process.argv[2]));
-let json1 = JSON.parse(fs.readFileSync(process.argv[3]));
+let file0 = process.argv[2];
+let file1 = process.argv[3];
+
+if (!file0 || !file1) {
+	log('error: file paths required');
+	log('$ json-combo /file/1.json /file/2.json');
+	return;
+}
+
+let json0 = JSON.parse(fs.readFileSync(file0));
+let json1 = JSON.parse(fs.readFileSync(file1));
 deepExtend(json0, json1);
-let file = process.argv[2].slice(0, -5) + '_combo.json';
-fs.writeFileSync(file, JSON.stringify(json0, null, '\t'));
-console.log(file);
+let outFile = file0.slice(0, -5) + '_combo.json';
+fs.writeFileSync(outFile, JSON.stringify(json0, null, '\t'));
+log("output: " + outFile);
